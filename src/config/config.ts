bdc6e7176c21a1e11aba64aa71fd0aa109858f45
Ed Angelis <ed4ngelis@gmail.com>
2023-10-13 14:09:39 -0300

    environment variables

---
+++ src/config/config.ts
@@ -0,0 +1,26 @@
+
+interface IProps {
+    domain: string
+}
+
+interface IConfig {
+    development: IProps
+    production: IProps
+}
+
+const config = {
+    development: {
+        domain: 'http://localhost:3000',
+    } as IProps,
+    production: {
+        domain: 'https://www.example.com',
+    } as IProps,
+}
+
+type ObjectIndex = keyof typeof config
+
+const environment: ObjectIndex = process.env.NEXT_PUBLIC_ENVIRONMENT as ObjectIndex || 'development'
+
+const env: IProps = config[environment] as IProps
+
+export {env}
